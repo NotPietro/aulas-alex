9ecd51c4d17ddc13d06c9c77905cbd2fe5189888
Pepe <128159900+NotPietro@users.noreply.github.com>
2024-03-15 08:42:57 -0300

    Add files via upload

---
+++ aulas-alex/aula1/typestring.js
@@ -0,0 +1,15 @@
+const texto1 = 'Olá, mundo!';
+const texto2 = 'Olá, mundo!';
+const senha = 'senhasupersegura123';
+const stringDeNumeros = '34567';
+
+//const citacao = 'O Leo disse "olá"';
+const citacao = 'Meu nome é ';
+const nome = 'Leo'
+//console.log(citacao)
+
+// concatenação (+)
+
+console.log(citacao + nome)
+
+//template string ou template literal
